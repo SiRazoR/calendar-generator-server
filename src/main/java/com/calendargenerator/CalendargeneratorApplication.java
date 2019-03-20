6c95b97f1256dd5af5412e7689ebfd69966ce819
test <test@test.com>
2019-03-20 23:03:21 +0100

    Initial commit

---
+++ src/main/java/com/calendargenerator/CalendargeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.calendargenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendargeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendargeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calendargenerator/calendargenerator/CalendargeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.calendargenerator.calendargenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendargeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
