b80c16be9a0153963e1ed9d372416185a092a1b3
SiRazoR <torecz@o2.pl>
2019-03-21 23:59:47 +0100

    & Created base project structure & Created scraper that generates list of classes of provided group ID & Created IcsCalendar class which generates calendar and provides functionality to remove unwanted classes & Created CalendarService and CalendarController

---
+++ src/main/java/com/calendargenerator/controller/CalendarController.java
@@ -0,0 +1,27 @@
+package com.calendargenerator.controller;
+
+import com.calendargenerator.service.CalendarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalendarController {
+
+    @Autowired
+    private CalendarService calendarService;
+
+    @RequestMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String greet() {
+        return "Add group id to URL above after /";
+    }
+
+    @RequestMapping(value = "/{groupId}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> schedule(@PathVariable String groupId) throws Exception {
+        return calendarService.getSchedule(groupId);
+    }
+}
+
---
+++ src/main/java/com/calendargenerator/service/CalendarService.java
@@ -0,0 +1,27 @@
+package com.calendargenerator.service;
+
+import com.calendargenerator.service.process.IcsCalendar;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalendarService {
+
+    public ResponseEntity<String> getSchedule(String groupId) throws Exception {
+        IcsCalendar calendar = new IcsCalendar(groupId);
+
+        HttpHeaders headers = new HttpHeaders() {{
+            add(HttpHeaders.ACCEPT_ENCODING, "gzip");
+            add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+            add(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+            add(HttpHeaders.EXPIRES, "0");
+            add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=test.ics");
+        }};
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(calendar.toString());
+    }
+}
